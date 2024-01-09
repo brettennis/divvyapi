482a95eab8d017397094fa7ebebc3fa0e8ceb78d
Brett Ennis <brettennis4@gmail.com>
2024-01-09 10:54:46 -0500

    deploy to vercel

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: 'brettennis',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
-    database: 'divvy',
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    // user: 'brettennis',
+    // password: 'password',
+    // host: 'localhost',
+    // port: 5432,
+    // database: 'divvy',
 })
 
 module.exports = pool;
