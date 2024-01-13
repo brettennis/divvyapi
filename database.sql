59a23b40e6133b4f23e94c41a6f2544e02632b81
Brett Ennis <brettennis4@gmail.com>
2024-01-12 20:29:21 -0500

    change database schema

--- database.sql
+++ database.sql
@@ -1,6 +1,25 @@
 CREATE DATABASE divvy;
 
-CREATE TABLE bill(
+CREATE TABLE bill (
     bill_id SERIAL PRIMARY KEY,
-    restaurant VARCHAR(255)
+    restaurant_name VARCHAR(150),       /* merchant_name */
+    restaurant_address VARCHAR(150),    /* merchant_address */
+    date_receipt TIMESTAMP,             /* date */
+    date_entered TIMESTAMP,             /* calculate now() */
+    tax_percent real,                   /* taxRate */
+    tip_percent real,                   /* tip */
+    payer_id integer REFERENCES patron (patron_id)
+);
+
+CREATE TABLE patron (
+    patron_id SERIAL PRIMARY KEY,
+    name_first VARCHAR(50),             /* nameFirst */
+    name_last VARCHAR(50),              /* nameLast */
+    phone VARCHAR(10)
+);
+
+CREATE TABLE bill_patron (
+    bill_id SERIAL REFERENCES bill,
+    patron_id SERIAL REFERENCES patron,
+    PRIMARY KEY (bill_id, patron_id)
 );
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 //
-// ROUTES
+// ROUTES: BILLS
 //
 
 // create bill
@@ -20,7 +20,9 @@ app.post('/bills', async (req, res) => {
     try {
         const { restaurant } = req.body;
         const newBill = await pool.query(
-            'INSERT INTO bill(restaurant) VALUES($1) RETURNING *', 
+            `INSERT INTO bill (restaurant) 
+                VALUES($1) 
+                RETURNING *`, 
             [restaurant]
         );
         res.json(newBill.rows[0]);
@@ -34,7 +36,7 @@ app.post('/bills', async (req, res) => {
 app.get('/bills', async (req, res) => {
     try {
         const allBills = await pool.query(
-            'SELECT * FROM bill'
+            `SELECT * FROM bill`
         );
         res.json(allBills.rows);
     } catch (err) {
@@ -48,7 +50,7 @@ app.get('/bills/:id', async (req, res) => {
     try {
         const { id } = req.params;
         const bill = await pool.query(
-            'SELECT * FROM bill WHERE bill_id = $1',
+            `SELECT * FROM bill WHERE bill_id = $1`,
             [id]
         );
         res.json(bill.rows[0]);
@@ -64,10 +66,11 @@ app.put('/bills/:id', async (req, res) => {
         const { id } = req.params;
         const { restaurant } = req.body;
         const updateBill = await pool.query(
-            "UPDATE bill SET restaurant = $1 WHERE bill_id = $2",
+            `UPDATE bill SET restaurant = $1 
+                WHERE bill_id = $2`,
             [restaurant, id]
         );
-        res.json("Bill was updated!");
+        res.json("Success");
     } catch (err) {
         console.error(err.message);
     }
@@ -79,13 +82,102 @@ app.delete("/bills/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const deleteBill = await pool.query(
-            "DELETE FROM bill WHERE bill_id = $1",
+            `DELETE FROM bill WHERE bill_id = $1`,
             [id]
         );
-        res.json("Bill was deleted!");
+        res.json("Success");
     } catch (err) {
         console.log(err.message);
     }
-  });
+})
 
-app.listen(3000, () => console.log(`App running on port 3000.`));
+
+//
+// ROUTES: PATRONS
+//
+
+// create patron
+
+app.post('/patrons', async (req, res) => {
+    try {
+        const b = req.body;
+        const newPatron = await pool.query(
+            `INSERT INTO patron 
+                (name_first, name_last, phone) 
+                VALUES ($1, $2, $3) 
+                RETURNING *`, 
+            [ b.name_first, b.name_last, b.phone ]
+        );
+        res.json(newPatron.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+// get all patrons
+
+app.get('/patrons', async (req, res) => {
+    try {
+        const allPatrons = await pool.query(
+            `SELECT * FROM patron`
+        );
+        res.json(allPatrons.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// get a patron
+
+app.get('/patrons/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const patron = await pool.query(
+            `SELECT * FROM patron 
+                WHERE patron_id = $1`,
+            [id]
+        );
+        res.json(patron.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// update a patron
+
+app.put('/patrons/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const b = req.body;
+        const updatePatron = await pool.query(
+            `UPDATE patron 
+                SET (name_first, name_last, phone) 
+                = ($1, $2, $3) 
+                WHERE patron_id = $4`,
+            [ b.name_first, b.name_last, b.phone, id ]
+        );
+        res.json("Success");
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// delete a patron
+
+app.delete("/patrons/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteBill = await pool.query(
+            `DELETE FROM patron 
+                WHERE patron_id = $1`,
+            [id]
+        );
+        res.json("Success");
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
+
+
+app.listen(3000, () => console.log('App running on port 3000.'));
